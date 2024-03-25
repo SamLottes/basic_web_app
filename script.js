b624f879acf453cd3040b1309da142e64ba5c4b1
Sam <99222234+SamLottes@users.noreply.github.com>
2024-03-24 22:17:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//get reference to the button in the HTML file
+const clickMeBtn = document.getElementById('clickMeBtn');
+
+//Add event listener to the button
+clickMeBtn.addEventListener('click', function () {
+    window.open('https://www.linkedin.com/in/samuel-lottes/');
+});
+
